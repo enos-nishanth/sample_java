b888861e24136b3ef3f479dc6c7c4cd157387669
Enos Nishanth J <134956804+enos-nishanth@users.noreply.github.com>
2026-03-04 21:25:45 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,15 @@
 public class Test {
 
-  static void add(){
+  static void sub(){
 
     int a=30;
     int b=70;
-    int result = a+b;
-    System.out.println("The sum is " +result);
+    int result = b-a;
+    System.out.println("The difference is " +result);
     
   }
   public static void main(String[] args)
   {
-    add();
+    sub();
   }
 }
